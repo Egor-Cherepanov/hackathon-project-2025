9c3bbf367b80dab66d22a23a2df0d68b647ea31a
Egor-Cherepanov <105498881+Egor-Cherepanov@users.noreply.github.com>
2025-04-19 09:28:37 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Наш проект</h1>
+      <div>
+        <h2>Наша команда</h2>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
