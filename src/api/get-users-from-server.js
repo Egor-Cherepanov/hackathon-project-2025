5f57dd8f3ac5239a0253f723a2c1284cd678719a
Zhenya <kerrigan.vig56@gmail.com>
2025-04-20 13:30:09 +0300

    Удаление лишнего

--- src/api/get-users-from-server.js
+++
@@ -1,5 +0,0 @@
-export const getUsersFromServer = (setStore) => {
-  fetch("http://localhost:3000/members")
-    .then((loadedData) => loadedData.json())
-    .then((noteData) => setStore(noteData));
-};
