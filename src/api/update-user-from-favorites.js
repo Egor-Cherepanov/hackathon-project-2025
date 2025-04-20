eff937ff7efedef122a9dec955c691b92c0b42b8
Zhenya <kerrigan.vig56@gmail.com>
2025-04-20 19:44:38 +0300

    Добавить кнопку Избранное

--- src/api/update-user-from-favorites.js
+++ src/api/update-user-from-favorites.js
@@ -1,4 +1,4 @@
-export const deleteUserFromFavorites = (id, value) => {
+export const updateUserFromFavorites = (id, value) => {
   fetch(`http://localhost:3000/members/${id}`, {
     method: "PATCH",
     headers: {
